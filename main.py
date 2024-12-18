e5a45d9b90d94a2542dcce1598f26c49850cd14c
kadir1212191 <abdulkadir.kalac@std.yeditepe.edu.tr>
2024-12-18 17:37:00 +0300

    python deneme15

---
+++ main.py
@@ -0,0 +1,7 @@
+sinav_1 = float(input("Birinci sınavın notunu girin: "))
+sinav_2 = float(input("İkinci sınavın notunu girin: "))
+istenen_ortalama = float(input("İstediğiniz ortalamayı girin: "))
+
+sinav_3 = 3 * istenen_ortalama - sinav_1 - sinav_2
+
+print("Üçüncü sınavda " ,sinav_3," almanız gerekiyor, böylece" ,istenen_ortalama, "ortalamayı elde edersiniz.")
