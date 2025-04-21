17f14a1025b56579519cd348b68495ea9105dcfc
kadir1212191 <abdulkadir.kalac@std.yeditepe.edu.tr>
2025-04-21 16:54:03 +0300

    Soru 5

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-kelime = input("Bir sözcük girin: ")
-
-sonuc = kelime[1:]
-print(sonuc)
+x = ("Bir sözcük giriniz :")
+y= int(input("Bir sayı girin :"))
+if y<len(x):
+  x1 = x[:y]
+  x2 = x[y:]
+  x=x1+"-"+x2
+  print(x)
