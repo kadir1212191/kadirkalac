82a30fb02c5530cb53af3407d3d802c74aed208c
kadir1212191 <abdulkadir.kalac@std.yeditepe.edu.tr>
2025-04-21 16:37:19 +0300

    SORU 1

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-hesap_bakiyesi = float(input("Başlangıç bakiyesini girin: "))
-islem = input("İşlem türünü girin (Para Yatırma / Para Çekme): ")
-tutar = float(input("İşlem tutarını girin: "))
-if islem == "Para Yatırma":
-  hesap_bakiyesi += tutar
-if islem == "Para Çekme":
-  hesap_bakiyesi -= tutar
-else:
- print("Geçersiz İşlem")
- 
-  
+x=input("Bir sözcük giriniz :")
+print(len(x))
