eefa94841dee7439da3529a7153313defba5f2c2
kadir1212191 <abdulkadir.kalac@std.yeditepe.edu.tr>
2025-04-28 18:17:16 +0300

    Update SORU 8 4.28.py

--- SORU 8 4.28.py
+++ SORU 8 4.28.py
@@ -1,5 +1,8 @@
-girdi = raw_input("Lütfen sayıları virgülle ayırarak girin: ")
-my_list = [int(sayi.strip()) for sayi in girdi.split(",")]
-cift_sayilar = [sayi for sayi in my_list if sayi % 2 == 0]
+list=[]
+for _ in range(5):
+ sayi=int(input("Lütfen sayı girin :"))
+ list.append(sayi)
 
-print "Çift sayılar:", cift_sayilar
+for x in list:
+  if (x%2) ==0:
+    print(x)
